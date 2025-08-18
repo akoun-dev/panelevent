316650bae5703fd99d932cf2b9f911334b6e1aa0
ABOA AKOUN BERNARD <aboa.akoun40@gmail.com>
2025-08-18 02:24:01 +0000

    feat: adjust Prisma client logging

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,8 +6,8 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+  (process.env.NODE_ENV === 'development'
+    ? new PrismaClient({ log: ['query'] })
+    : new PrismaClient());
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
